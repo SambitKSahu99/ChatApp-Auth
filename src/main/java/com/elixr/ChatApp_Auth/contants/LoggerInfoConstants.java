7ae1735d27498945c7118c5bb244426dd84c1710
Sambit Kumar Sahu <sambit.sahu@theelixrlabs.com>
2024-10-09 17:34:23 +0530

    updated code

--- src/main/java/com/elixr/ChatApp_Auth/contants/LoggerInfoConstants.java
+++ src/main/java/com/elixr/ChatApp_Auth/contants/LoggerInfoConstants.java
@@ -1,11 +1,9 @@
 package com.elixr.ChatApp_Auth.contants;
 
 public class LoggerInfoConstants {
-    public static final String ERROR_LOGOUT = "Some error occurred while logging out";
     public static final String USER_LOGIN = "User '{}' has logged in successfully";
     public static final String USER_LOGOUT = "User has logged out successfully";
     public static final String USER_UNAUTHORISED = "User '{}' is unauthorised";
-    public static final String RETRIEVING_USER_INFO = "Retrieved {} from database";
     public static final String VERIFYING_TOKEN = "Verifying token";
     public static final String TOKEN_VERIFICATION_SUCCESS = "Token successfully verified";
 }
